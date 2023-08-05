7ca75f864f970d7c46f0cb2348bd3749338fd4dc
Jim <61350333+wllilerjimw@users.noreply.github.com>
2023-08-05 18:14:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,33 +5,12 @@ import pandas as pd
 import streamlit as st
 
 """
-import streamlit as st
+def propose(date):
+  return "陈燕,嫁给我吧" 
 
-# BMI计算函数
-def calculate_bmi(height, weight):
-  bmi = weight / (height ** 2)
-  if bmi < 18.5:
-    return "过轻"
-  elif bmi < 25:
-    return "正常"
-  elif bmi < 28:
-    return "过重"
-  elif bmi < 32:
-    return "肥胖"
-  else:
-    return "严重肥胖"
+x = input("请输入我们的恋爱纪念日时间:")
 
-# 主函数  
-if __name__ == '__main__':
-
-  st.title("小惊喜哦,请年月日输清楚")
-
-  date = st.text_input("请输入恋爱纪念日")
-
-  if date == "2021年6月8日":
-    st.write("陈燕,嫁给我吧")
-
-  else:
-    st.write("日期错误,请重新输入")
-    
-
+if x == "2021年6月8日":
+  print(propose(x))
+else:
+  print("日期输入错误,请重新输入!")
