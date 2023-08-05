34cf55a0ff4cebddadd376b2087a4ae0512712c3
Jim <61350333+wllilerjimw@users.noreply.github.com>
2023-08-05 18:17:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,22 @@ import pandas as pd
 import streamlit as st
 
 """
+# Welcome my baby!
+
+这是一份惊喜，请输入正确日期，你便能看到哦！
+"""
+import streamlit as st
+
 def propose(date):
-  return "陈燕,嫁给我吧" 
+  return "陈燕,嫁给我吧"
 
-x = input("请输入我们的恋爱纪念日时间:")
+if __name__ == '__main__':
 
-if x == "2021年6月8日":
-  print(propose(x))
-else:
-  print("日期输入错误,请重新输入!")
+    st.title("小惊喜哦，请年月日输清楚")
+    
+    date = st.text_input("请输入恋爱纪念日")  
+
+    if date == "2021年6月8日":
+        st.write(propose(date))
+    else:
+        st.write("日期错误,请重新输入")
