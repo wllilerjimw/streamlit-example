4c6e162928dd9df30e084b51b1efbccb3c6e3f4b
Jim <61350333+wllilerjimw@users.noreply.github.com>
2023-08-05 17:45:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,9 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+# Welcome my baby!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+这是一份惊喜，请输入正确日期，你便能看到哦！
 """
 import streamlit as st
 
